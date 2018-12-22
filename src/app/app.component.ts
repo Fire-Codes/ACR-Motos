759f9aa35400a656eb9c3eabc2f3c38f03e18e30
Edycar Reyes <edycarreyes@gmail.com>
2018-12-21 23:20:04 -0600

    se hace un console log desde el app.component.ts para verificar si la aplicacion esta funcionando correctamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ACRMotos';
+  constructor() {  }
+
+  ngOnInit() {
+    console.log('Aplicacion Inicializada');
+  }
+
 }
