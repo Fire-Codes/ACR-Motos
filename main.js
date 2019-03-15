e4db01c20df9d5a4136bdfdfe09acded41120617
Edycar Reyes <edycarreyes@gmail.com>
2019-03-14 23:53:08 -0600

    se cambia la direccion de ejecucion de electron

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({ width: 800, height: 600 });
     // and load the index.html of the app. 
-    win.loadFile('./dist/ACRMOTOS/index.html');
+    win.loadFile('./dist/ACRMotos/index.html');
     // Open the DevTools.
     //win.webContents.openDevTools();
 
